1c5c577a373cc24a5545189353976c0bd83e01bd
Anton Bezborody <anton.bezborody@gmail.com>
2024-09-03 00:31:29 +0300

    init: react, mantine ui, fsd

---
+++ src/widgets/header/index.tsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.css';
+
+export const Header = () => {
+  return (
+    <div className={styles.header}>
+      Today
+      <div>datepicker</div>
+      <div>Profile</div>
+    </div>
+  );
+};
